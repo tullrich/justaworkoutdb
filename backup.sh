228601262264637cd3150828428d4e896138261d
Tim Ullrich <timothy.ullrich@gmail.com>
2015-10-15 21:54:45 -0500

    More useful backup.

--- backup.sh
+++ backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-scp wwwhost@fitness.tullrich.com:/tmp/justaworkout.db ./backup/justaworkout$(date "+_%Y_%m_%d.db")
+scp wwwhost@fitness.tullrich.com:/tmp/justaworkout.db ./backup/justaworkout.db
+cp ./backup/justaworkout.db ./backup/justaworkout$(date "+_%Y_%m_%d.db")
